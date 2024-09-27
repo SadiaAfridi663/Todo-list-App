58ba702002c3e02db5faa036e9eb463cd7a98d7f
Sadia Afridi <sadiaafridi663@gmail.com>
2024-09-27 14:31:07 +0500

    Create a new list item element

---
+++ index.js
@@ -0,0 +1,17 @@
+const taskInput  = document.getElementById("input")
+const Addbtn  = document.getElementById("add-btn")
+const taskList  = document.querySelector("ul")
+// console.log(taskList)
+Addbtn.addEventListener('click', function () {
+    if(!taskInput.value.trim()){
+        return alert("please add something")
+    }
+    const li = document.createElement("li")
+    taskList.appendChild(li);
+    const text = document.createElement("span")
+    li.appendChild(text)
+    var inputValue = taskInput.value
+    text.innerHTML = inputValue
+    li.setAttribute('class', "list")
+
+})
