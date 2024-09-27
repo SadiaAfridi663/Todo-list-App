a63319f598687af46438fdd04f2c4b394d5e10cc
Sadia Afridi <sadiaafridi663@gmail.com>
2024-09-27 18:05:57 +0500

    Clear input field after adding a task

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const taskInput  = document.getElementById("input")
-const Addbtn  = document.getElementById("add-btn")
-const taskList  = document.querySelector("ul")
+const taskInput = document.getElementById("input")
+const Addbtn = document.getElementById("add-btn")
+const taskList = document.querySelector("ul")
 // console.log(taskList)
 Addbtn.addEventListener('click', function () {
-    if(!taskInput.value.trim()){
+    if (!taskInput.value.trim()) {
         return alert("please add something")
     }
     const li = document.createElement("li")
@@ -14,29 +14,30 @@ Addbtn.addEventListener('click', function () {
     text.innerHTML = inputValue
     li.setAttribute('class', "list")
 
-     var firstitem = taskList.firstChild;
-     taskList.insertBefore(li, firstitem); 
+    var firstitem = taskList.firstChild;
+    taskList.insertBefore(li, firstitem);
 
-     // container for  buttons (complete & delete)
+    // container for  buttons (complete & delete)
     const btnContainer = document.createElement("div")
     li.appendChild(btnContainer)
     btnContainer.setAttribute("class", "btn-container")
 
-     // Complete Button
-     const CompleteButton = document.createElement("button");
-     btnContainer.appendChild(CompleteButton)
-     CompleteButton.innerHTML = '<i class="fas fa-check fa-lg"></i>'
-     CompleteButton.setAttribute('class', 'complete-btn')
- 
-     // remove button
-     const removeButton = document.createElement("button");
-     removeButton.innerHTML = '<i class="fas fa-trash fa-lg"></i>'
-     removeButton.setAttribute('class', 'remove-btn')
-     btnContainer.appendChild(removeButton)
-     removeButton.addEventListener('click', function() {
-         taskList.removeChild(li)
-     });
-     CompleteButton.addEventListener('click', function(){
-         text.classList.toggle("completed")
-     })
+    // Complete Button
+    const CompleteButton = document.createElement("button");
+    btnContainer.appendChild(CompleteButton)
+    CompleteButton.innerHTML = '<i class="fas fa-check fa-lg"></i>'
+    CompleteButton.setAttribute('class', 'complete-btn')
+
+    // remove button
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = '<i class="fas fa-trash fa-lg"></i>'
+    removeButton.setAttribute('class', 'remove-btn')
+    btnContainer.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+        taskList.removeChild(li)
+    });
+    CompleteButton.addEventListener('click', function () {
+        text.classList.toggle("completed")
+    })
+    taskInput.value = ""
 })
