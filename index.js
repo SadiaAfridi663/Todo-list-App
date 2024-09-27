5f12fe8d1f1fc1d1e596edb40abcf79904125baa
Sadia Afridi <sadiaafridi663@gmail.com>
2024-09-27 17:49:20 +0500

    create and style complete and remove button

--- index.js
+++ index.js
@@ -17,4 +17,26 @@ Addbtn.addEventListener('click', function () {
      var firstitem = taskList.firstChild;
      taskList.insertBefore(li, firstitem); 
 
+     // container for  buttons (complete & delete)
+    const btnContainer = document.createElement("div")
+    li.appendChild(btnContainer)
+    btnContainer.setAttribute("class", "btn-container")
+
+     // Complete Button
+     const CompleteButton = document.createElement("button");
+     btnContainer.appendChild(CompleteButton)
+     CompleteButton.innerHTML = '<i class="fas fa-check fa-lg"></i>'
+     CompleteButton.setAttribute('class', 'complete-btn')
+ 
+     // remove button
+     const removeButton = document.createElement("button");
+     removeButton.innerHTML = '<i class="fas fa-trash fa-lg"></i>'
+     removeButton.setAttribute('class', 'remove-btn')
+     btnContainer.appendChild(removeButton)
+     removeButton.addEventListener('click', function() {
+         taskList.removeChild(li)
+     });
+     CompleteButton.addEventListener('click', function(){
+         text.classList.toggle("completed")
+     })
 })
